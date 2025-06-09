2d5772ff92f0ad170acf690f0dc53a8876e80b4c
nehemiah313 <60307321+nehemiah313@users.noreply.github.com>
2025-06-09 02:57:56 -0400

    Improve trust score weighting

--- src/components/TaskRoutingDashboard.tsx
+++ src/components/TaskRoutingDashboard.tsx
@@ -1,4 +1,5 @@
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
